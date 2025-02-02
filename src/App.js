1fabe967095a1f5e7fab2fef16f936039a180835
Batyrai <batyrai@mail.ru>
2025-02-02 22:41:51 +0300

    init

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import './App.css';
 import {useEffect} from "react";
 import {useTelegram} from "./hooks/useTelegram";
 import Header from "./components/Header/Header";
-const tg = window.Telegram.WebApp;
+import {Route, Routes} from 'react-router-dom'
+import ProductList from "./components/ProductList/ProductList";
+import Form from "./components/Form/Form"
+
+// const tg = window.Telegram.WebApp;
 
 function App() {
     const {onToggleButton, tg} = useTelegram();
@@ -14,7 +18,12 @@ function App() {
 
     return (
     <div className="App">
-        <button onClick={onToggleButton}>toggle</button>
+        <Header />
+            <Routes>
+                <Route index element={<ProductList />}/>
+                <Route path={'form'} element={<Form />}/>
+
+            </Routes>
     </div>
   );
 }
