9d2782e13786d6c0b177879554cc6ef6d12f415b
Batyrai <batyrai@mail.ru>
2025-02-02 16:52:54 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import {useEffect} from "react";
+
 const tg = window.Telegram.WebApp;
 
 function App() {
@@ -9,9 +10,7 @@ function App() {
           tg.ready();
           }, [])
 
-    const onClose = () => {
-        tg.close()
-    }
+
     return (
     <div className="App">
       work
