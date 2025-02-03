e4b40f124b77cb72e7b1fb8cbe08af7ff1d92116
Batyrai <batyrai@mail.ru>
2025-02-03 21:11:38 +0300

    init

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import './ProductList.css'
 import ProductItem from "../ProductItem/ProductItem";
 import {useTelegram} from "../../hooks/useTelegram";
-
 const products = [
     {id: '1', title: 'Наушники №1', price: 300, description: 'Черного цвета'},
     {id: '1', title: 'Наушники №1', price: 300, description: 'Черного цвета'},
@@ -22,7 +21,30 @@ const getTotalPrice = (items) => {
 
 const ProductList = () => {
     const [addedItems, setAddedItems] = useState([]);
-    const {tg} = useTelegram();
+    const {tg, queryId} = useTelegram();
+     const onSendData = useCallback( () => {
+        const data = {
+            products: addedItems,
+            totalPrice: getTotalPrice(addedItems),
+            queryId,
+        }
+
+        fetch('http://localhost:8000', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data)
+        })
+    }, [])
+
+    useEffect(() => {
+        tg.onEvent('mainButtonClicked', onSendData)
+           return () =>{
+            tg.offEvent('mainButtonClicked', onSendData)
+           }
+
+    }, [onSendData])
     const onAdd = (product) => {
         const alreadyAdded = addedItems.find(item => item.id === product.id)
         let newItems = [];
