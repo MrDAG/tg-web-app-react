58c45b852d725bcbd893ad9d30cfc45758ac15e6
Batyrai <batyrai@mail.ru>
2025-02-04 11:37:20 +0300

    init

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -5,14 +5,14 @@ import {useTelegram} from "../../hooks/useTelegram";
 import {useCallback, useEffect} from "react";
 
 const products = [
-    {id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '2', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая'},
-    {id: '3', title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '4', title: 'Куртка 8', price: 122, description: 'Зеленого цвета, теплая'},
-    {id: '5', title: 'Джинсы 3', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '6', title: 'Куртка 7', price: 600, description: 'Зеленого цвета, теплая'},
-    {id: '7', title: 'Джинсы 4', price: 5500, description: 'Синего цвета, прямые'},
-    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
+    {id: '1', title: 'Наушники Redmi', price: 700, description: 'Синего цвета'},
+    {id: '2', title: 'Наушники Perfeo ALPHA', price: 90, description: 'Зеленого цвета'},
+    {id: '3', title: 'Defender Basic 210', price: 700, description: 'Синего цвета'},
+    {id: '4', title: 'Наушники Perfeo ALPHA ', price: 200, description: 'Зеленого цвета'},
+    {id: '5', title: 'Наушники Smartbuy S6', price: 600, description: 'Синего цвета'},
+    {id: '6', title: 'Наушники GAL HM-085', price: 600, description: 'Зеленого цвета'},
+    {id: '7', title: 'Наушники GAL HM-085', price: 500, description: 'Синего цвета'},
+    {id: '8', title: 'Наушники Red Line S3', price: 400, description: 'Зеленого цвета'},
 ]
 
 const getTotalPrice = (items = []) => {
